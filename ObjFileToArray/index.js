8bfd9033cfb55935a61d185d0262d6e205aca6fb
ximet <dq.ximet@gmail.com>
2017-02-05 13:28:56 +0300

    update parser .obj file

---
+++ ObjFileToArray/index.js
@@ -0,0 +1,19 @@
+const fs = require("fs")
+const { parseOBJ } = require("./parser.js")
+
+const parseObjFile = () => {
+    parseOBJ(fs.createReadStream("model.obj"), function(err, result) {
+      if(err) {
+        throw new Error("Error parsing OBJ file: " + err)
+      }
+
+      console.log(result);
+      const wstream = fs.createWriteStream('output.txt');
+      wstream.write(JSON.stringify(result));
+      wstream.end();
+  })
+}
+
+module.exports = {
+  parseObjFile
+};
--- app.js
+++ app.js
@@ -1,13 +1,2 @@
 const fs = require("fs")
-const { parseOBJ } = require("./parser.js")
-
-parseOBJ(fs.createReadStream("model.obj"), function(err, result) {
-    if(err) {
-      throw new Error("Error parsing OBJ file: " + err)
-    }
-
-    console.log(result);
-    const wstream = fs.createWriteStream('output.txt');
-    wstream.write(JSON.stringify(result));
-    wstream.end();
-})
+const { parseObjFile } = require("./ObjFileToArray/index.js")
